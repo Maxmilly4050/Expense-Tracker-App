fd05c6b2738da60ca6632df6ab6134bf1b076a7b
Maxmilly4050 <maxsteel4050@gmail.com>
2026-01-10 22:58:47 +0300

    Adding of Categories and Date picker

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,6 +1,6 @@
-import 'dart:math';
-
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget{
   const NewExpense({super.key});
@@ -12,19 +12,39 @@ class NewExpense extends StatefulWidget{
 }
 
 class _NewExpenseState extends State<NewExpense>{
-  var _enteredTitle = '';
 
-  void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  final _dateFormatter = DateFormat.yMd();
+  Category _selectedCategory = Category.food;
+
+  @override
+  void dispose(){
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
   }
-
+  
+  void _presentDatePicker() async{
+    final pickedDate = await showDatePicker(
+      context: context, 
+      initialDate: DateTime.now(), 
+      firstDate: DateTime(DateTime.now().year - 1), 
+      lastDate: DateTime.now(),
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Padding(padding: EdgeInsets.all(16),
                     child: Column(
                       children: [
                         TextField(
-                          onChanged: _saveTitleInput,
+                          controller: _titleController,
                           maxLength: 50,
                           keyboardType: TextInputType.text,
                           decoration: const InputDecoration(
@@ -33,10 +53,61 @@ class _NewExpenseState extends State<NewExpense>{
                         ),
                         Row(
                           children: [
+                            Expanded(child:   TextField(
+                              controller: _amountController,
+                              decoration: const InputDecoration(
+                                prefixText: '\$ ',
+                                labelText: 'Amount',
+                            ),
+                            )),
+                            const SizedBox(width: 16,),
+                            Expanded(child:   Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Text(_selectedDate == null ? 'No date chosen' : _dateFormatter.format(_selectedDate!)),
+                                IconButton(onPressed: (){
+                                  _presentDatePicker();
+                                }, icon: const Icon(Icons.calendar_month)),
+                              ],
+                            )
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16,),
+                        Row(
+                          children: [
+                            DropdownButton(
+                              value: _selectedCategory,
+                              items: Category.values.map(
+                              (category) {
+                                return DropdownMenuItem(
+                                  value: category,
+                                  child: Text(
+                                    category.name.toUpperCase(),
+                                  ),
+                                );
+                              },
+                            ).toList(
+                            ), onChanged: (value){
+                              if (value == null){
+                                return;
+                              }
+
+                              setState(() {
+                                _selectedCategory = value!;
+                              });
+                            }),
+                            Spacer(),
                             ElevatedButton(onPressed: (){
-                              print(_enteredTitle);
+                              // ignore: avoid_print
+                              print(" Title: ${_titleController.text}, Amount: ${_amountController.text} ");
                             },
-                            child: Text('Submit expense'))
+                            child: Text('Submit expense')),
+                            ElevatedButton.icon(onPressed: (){
+                              Navigator.pop(context);
+                            }, 
+                            icon: Icon(Icons.close_outlined), 
+                            label: Text('Cancel'))
                           ],
                         )
                       ],
